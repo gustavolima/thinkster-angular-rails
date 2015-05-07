2179cdde0ad42a77ba68983afc5ed26cd28d0219
gustavolima <degustavinho@gmail.com>
2015-05-07 15:21:45 -0300

    Creating Routes and Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  resources :posts, only: [:create, :index, :show] do
+    resources :comments, only: [:show, :create] do
+      member do
+        put '/upvote' => 'comments#upvote'
+      end
+    end
+
+    member do
+      put '/upvote' => 'posts#upvote'
+    end
+  end
+
   root to: 'pages#angular'
 end
+
